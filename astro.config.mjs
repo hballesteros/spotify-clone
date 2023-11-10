18c8e7aa47e8d8a6f472905ca3fc5d87eb7b2f1e
Hugo <hugo@boahstudio.com>
2023-11-09 22:59:35 -0300

    playlistCard and playlist page

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  // esto se pone para que las páginas se rendericen en el servidor
+  output: 'server' 
 });
