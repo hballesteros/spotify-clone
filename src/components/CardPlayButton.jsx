363f29d7a3641eb15b28b4e78e248c5d98bef4c7
Hugo <hugo@boahstudio.com>
2023-11-13 05:54:14 -0300

    Music, CardPlay and volume

--- src/components/CardPlayButton.jsx
+++ src/components/CardPlayButton.jsx
@@ -1,11 +1,44 @@
-import { Pause, Play } from './Player';
+import { Pause, Play } from './Player'
+import { usePlayerStore } from '@/store/playerStore'
 
 export function CardPlayButton({ id }) {
-  return (
-    <div className="card-play-button">
-      <button className="bg-green-500 rounded-full p-4">
-        <Play className="w-5 h-5" />
-      </button>
-    </div>
-  );
+
+    const { currentMusic, isPlaying, setIsPlaying, setCurrentMusic } = usePlayerStore(state => state)
+
+    const isPlayingPlaylist = isPlaying && currentMusic?.playlist.id === id
+
+    const handleClick = () => {
+
+        if (isPlayingPlaylist) {
+            setIsPlaying(false)
+            return
+        }
+
+        //promises
+        fetch(`/api/get-info-playlist.json?id=${id}`)
+            .then(res => res.json())
+            .then(data => {
+                const { songs, playlist } = data
+                setIsPlaying(true)
+                setCurrentMusic({ songs, playlist, song: songs[0] })
+
+                console.log({ songs, playlist })
+            })
+
+        // async await
+        // const res = await fetch(`/api/get-info-playlist.json?id=${id}`)
+        // const data = await res.json()
+        // const { songs, playlist } = data
+    }
+
+    return (
+        <div className="card-play-button">
+        <button 
+            className="bg-green-500 rounded-full p-4"
+            onClick={handleClick}
+        >
+            {isPlayingPlaylist ? <Pause className="w-4 h-4" /> : <Play className="w-4 h-4" />}
+        </button>
+        </div>
+    );
 }
